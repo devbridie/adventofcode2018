3056d5dbbde787e2a56f99ac2177bd0ec4d4efc4
Dereck Bridie <me@devbridie.com>
2018-12-04 16:35:45 +0900

    Remove timing

--- src/main/kotlin/com/devbridie/adventofcode2018/Main.kt
+++ src/main/kotlin/com/devbridie/adventofcode2018/Main.kt
@@ -2,18 +2,19 @@ package com.devbridie.adventofcode2018
 
 import com.devbridie.adventofcode2018.solutions.day1
 import com.devbridie.adventofcode2018.solutions.day2
-import kotlin.system.measureTimeMillis
+import com.devbridie.adventofcode2018.solutions.day3
+import com.devbridie.adventofcode2018.solutions.day4
 
 fun main(args: Array<String>) {
-    val days = listOf(day1, day2)
+    val days = listOf(day1, day2, day3, day4)
     val day = selectDay(days)
     executeDay(day)
 }
 
 fun executeDay(day: Day) {
     val input = day.loadInput()
-    measureTimeMillis { print("Part 1: ${day.part1(input)}") }.also { println(" -- time: ${it}ms") }
-    measureTimeMillis { print("Part 2: ${day.part2(input)}") }.also { println(" -- time: ${it}ms") }
+    println("Part 1: ${day.part1(input)}")
+    println("Part 2: ${day.part2(input)}")
 }
 
 private fun selectDay(day: Iterable<Day>): Day {
